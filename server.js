70258c47497a72ca75f76ef3f198cec330914bf4
brenda70904 <brendaj0214@gmail.com>
2022-11-21 16:05:36 -0800

    switching driver

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-'use strict';
 
 require('dotenv').config();
 const express = require('express');
@@ -7,12 +6,32 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
+const mongoose = require('mongoose');
+const Book =require('./models/Books');
+
 const PORT = process.env.PORT || 3001;
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  console.log('Mongoose is connected');
+});
+
+mongoose.connect(process.env.DB_URL);
+
 app.get('/test', (request, response) => {
+  response.send('test request received');
+});
 
-  response.send('test request received')
+app.get('/books',getBooks);
 
-})
+async function getBooks(req, res, next) {
+  try {
+    let result = await Book.find();
+    res.send(result);
+  } catch (error) {
+  next(error); 
+  }
+}
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
